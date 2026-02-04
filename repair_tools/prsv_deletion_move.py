6d19f5c33766cb5b9ce8404266bf7f912c85db24
Emilee Buytkins <86252603+ebuytkin@users.noreply.github.com>
2026-02-04 14:16:59 -0500

    Remove credential choices from argument parser
    Removed specific credential choices from argument parser.

--- repair_tools/prsv_deletion_move.py
+++ repair_tools/prsv_deletion_move.py
@@ -14,7 +14,6 @@ def parse_args():
         "--credentials",
         type=str,
         required=True,
-        choices=["test-ingest", "prod-ingest", "test-manage"],
         help="which set of credentials to use",
         )
     
